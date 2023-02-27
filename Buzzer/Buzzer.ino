c94952e209b5cc6ab5b70e92286fcfe9ed1d3b86
jonnjonnjo <zhenagajo234@gmail.com>
2023-02-27 19:27:16 +0700

    add all

--- Buzzer/Buzzer.ino
+++ Buzzer/Buzzer.ino
@@ -1,115 +1,25 @@
-/*
-  Color memory game for ESP32
-    Made by Fawwaz Abrial Saffa
-
-  Referenced: https://wokwi.com/projects/328451800839488084
-*/ 
-
 #include "pitches.h"
+#include "Tone.h"
+#define BUZZZER_PIN  18 // ESP32 pin GIOP18 connected to piezo buzzer
 
-// Color order of index: RED, YELLOW, GREEN, BLUE
-#define NUM_OF_COLORS 4
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
 
-// set pin numbers
-#define BUZZER_PIN 14
-const int ledPins[] = {18, 19, 25, 26};
-const int buttonPins[] = {4, 5, 34, 35};
-
-// game variables + constants
-#define MAX_GAME_LENGTH 30
-const int gameTones[] = { NOTE_G3, NOTE_C4, NOTE_E4, NOTE_G5};
-int gameSequence[MAX_GAME_LENGTH] = {0};
-int gameIndex = 0;
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
 
 void setup() {
-  Serial.begin(115200);  
-  // Set GPIO Pin for input and output
-  for (int i=0; i<NUM_OF_COLORS; i++) {
-    pinMode(buttonPins[i], INPUT);
-    pinMode(ledPins[i], OUTPUT);
-  }
-  pinMode(BUZZER_PIN, INPUT);
-}
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(BUZZZER_PIN, melody[thisNote], noteDuration);
 
-int readButtons() {
-  while (true) {
-    for (int i=0; i<NUM_OF_COLORS; i++) {
-      if (digitalRead(buttonPins[i]) == HIGH) return i;
-    }
-    delay(1);
-  }
-}
-
-void lightLEDAndPlayTone(int ledIndex) {
-  digitalWrite(ledPins[ledIndex], HIGH);
-  tone(BUZZER_PIN, gameTones[ledIndex]);
-
-  delay(300);
-  digitalWrite(ledPins[ledIndex], LOW);
-  noTone(BUZZER_PIN);
-}
-
-bool checkUserSequence() {
-  for (int i=0; i<gameIndex; i++) {
-    int expectedButton = gameSequence[i];
-    int actualButton = readButtons();
-    lightLEDAndPlayTone(actualButton);
-    if (expectedButton != actualButton) return false;
-  }
-  return true;
-}
-
-void gameOver() {
-  gameIndex = 0;
-  delay(200);
-
-  // Play a Wah-Wah-Wah-Wah sound
-  tone(BUZZER_PIN, NOTE_DS5);
-  delay(300);
-  tone(BUZZER_PIN, NOTE_D5);
-  delay(300);
-  tone(BUZZER_PIN, NOTE_CS5);
-  delay(300);
-  for (byte i = 0; i < 10; i++) {
-    for (int pitch = -10; pitch <= 10; pitch++) {
-      tone(BUZZER_PIN, NOTE_C5 + pitch);
-      delay(5);
-    }
-  }
-  noTone(BUZZER_PIN);
-}
-
-void playLevelUpSound() {
-  tone(BUZZER_PIN, NOTE_E4);
-  delay(150);
-  tone(BUZZER_PIN, NOTE_G4);
-  delay(150);
-  tone(BUZZER_PIN, NOTE_E5);
-  delay(150);
-  tone(BUZZER_PIN, NOTE_C5);
-  delay(150);
-  tone(BUZZER_PIN, NOTE_D5);
-  delay(150);
-  tone(BUZZER_PIN, NOTE_G5);
-  delay(150);
-  noTone(BUZZER_PIN);
-}
-
-void playSequence() {
-  for (int i=0; i<gameIndex; i++) {
-    lightLEDAndPlayTone(gameSequence[i]);
-    delay(50);
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(BUZZZER_PIN);
   }
 }
 
 void loop() {
-  Serial.println("\nSkor: " + gameIndex);
-  gameSequence[gameIndex++] = random(0, 4);
-  playSequence();
-  if (!checkUserSequence() gameOver();
-  delay(300);
-  if (gameIndex > 0) {
-    playLevelUpSound();
-    delay(300);
-  }
 }
